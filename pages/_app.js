0b583ac658a11f10693117e735ca725513b0fc9d
jose.gamez.poza <jose.gamez.poza@nttdata.com>
2022-02-11 13:36:47 +0100

    add config to app

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,11 @@
 import '../styles/globals.css'
+import {getLibrary} from '../config/web3'
+import {Web3ReactProvider} from '@web3-react/core'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  <Web3ReactProvider getLibrary={getLibrary}>
+     return <Component {...pageProps} />
+  </Web3ReactProvider>
 }
 
 export default MyApp
